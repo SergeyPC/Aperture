fb6a33de9bb227269cd623a40be1d0b49f10c528
SergeyPC <moefedorovna@gmail.com>
2024-01-31 20:01:29 +0200

    homework 12

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:5
+        }
+    }
 });
